4bb8e0541c3a8aa8f01c8e451a492fe6a87bfd3e
Dan Merritt <djosme@gmail.com>
2014-12-11 23:08:48 +0900

    Include missing access flag values for fields

--- dc.c
+++ dc.c
@@ -9,13 +9,14 @@
 
 /* Access flags */
 #define ACC_PUBLIC		0x0001	/* Declared public; may be accessed from outside its package. */
-#define ACC_FINAL		0x0010	/* Declared final; no subclasses allowed. */
-#define ACC_SUPER		0x0020	/* Treat superclass methods specially when invoked by the invokespecial instruction. */
-#define ACC_INTERFACE	0x0200	/* Is an interface, not a class. */
-#define ACC_ABSTRACT	0x0400	/* Declared abstract; must not be instantiated. */
+#define ACC_PRIVATE		0x0002	/* Declared private; usable only within the defining class. */
+#define ACC_PROTECTED	0x0004	/* Declared protected; may be accessed within subclasses. */
+#define ACC_STATIC		0x0008	/* Declared static. */
+#define ACC_FINAL		0x0010	/* Declared final; never directly assigned to after object construction (JLS §17.5). */
+#define ACC_VOLATILE	0x0040	/* Declared volatile; cannot be cached. */
+#define ACC_TRANSIENT	0x0080	/* Declared transient; not written or read by a persistent object manager. */
 #define ACC_SYNTHETIC	0x1000	/* Declared synthetic; not present in the source code. */
-#define ACC_ANNOTATION	0x2000	/* Declared as an annotation type. */
-#define ACC_ENUM		0x4000	/* Declared as an enum type. */
+#define ACC_ENUM		0x4000	/* Declared as an element of an enum. */
  
 /* SS4.7 - Attributes */
 typedef struct attribute_info_tag {
